98185b2726d8ddddb6a71d033b4887d06415ccfb
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-11 00:51:48 +0300

    floq drive init

---
+++ src/test/java/com/example/floqdrive/FloqDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floqdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloqDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
