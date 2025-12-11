f349037acf45cc6e6c0a65ca853b35e1dc927a09
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-11 15:13:59 +0300

    feat: init user entity

---
+++ src/main/java/com/floqdrive/entity/User.java
@@ -0,0 +1,26 @@
+package com.floqdrive.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User
+{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<FileEntity> files;
+}
