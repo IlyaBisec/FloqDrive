5c62fd905aed8c95261a96203beaa41776537419
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-11 15:14:39 +0300

    feat: init floq drive app

---
+++ src/main/java/com/floqdrive/FloqDriveApplication.java
@@ -0,0 +1,13 @@
+package com.floqdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloqDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloqDriveApplication.class, args);
+    }
+
+}
