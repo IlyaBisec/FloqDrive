77e7841b73b28b67ebb625e81f256c7e8ce7c466
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-14 04:41:46 +0300

    feat; init file repository

--- src/main/java/com/floqdrive/repository/UserRepository.java
+++ src/main/java/com/floqdrive/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.floqdrive.repository;
 
-public class UserRepository {
+import com.floqdrive.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/floqdrive/service/FileStorageService.java
+++ src/main/java/com/floqdrive/service/FileStorageService.java
@@ -1,4 +1,28 @@
 package com.floqdrive.service;
 
-public class FileStorageService {
+import com.floqdrive.dto.FileIndoDto;
+import com.floqdrive.dto.FileUploadResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+// Service for work with user files
+// load, get, delete and list files
+public interface FileStorageService
+{
+    // Load user file:
+    //  - generate uniq file name
+    //  - save file in disk
+    //  - create rec in database
+    FileUploadResponse uploadFile(MultipartFile file, Long userId);
+
+    // Load files for download
+    Resource loadFile(Long fileId, Long userId);
+
+    // Delete user file
+    void deleteFile(Long fileId, Long userId);
+
+    // Return user file list
+    List<FileIndoDto> listFiles(Long userId);
 }
