2ba87c7285a1aeb7dd23fe48a7f7fa0b9e5e5d7c
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 02:58:31 +0300

    feat: add register request

--- src/main/java/com/floqdrive/dto/RegisterRequest.java
+++ src/main/java/com/floqdrive/dto/RegisterRequest.java
@@ -1,4 +1,21 @@
 package com.floqdrive.dto;
 
-public class RegisterRequest {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// For new user register
+@Getter
+@Setter
+public class RegisterRequest
+{
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be 3-20 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 50, message = "Password must be 5-60 characters")
+    private String password;
 }
