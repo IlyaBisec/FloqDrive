204c99553fdb5347fd1146d11aa6f4e5b870c688
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 02:59:01 +0300

    feat: add jwt provider

--- src/main/java/com/floqdrive/security/JwtProvider.java
+++ src/main/java/com/floqdrive/security/JwtProvider.java
@@ -1,4 +1,45 @@
 package com.floqdrive.security;
 
-public class JwtProvider {
+import com.floqdrive.entity.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.security.Key;
+
+@Component
+public class JwtProvider
+{
+    private final String SECRET = "floqsecretkeyfloqsecretkeyfloqsecretkey123";
+    private final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 hours
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    // Token generation
+    public String generateToken(User user)
+    {
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Get userId from token
+    public Long getUserIdFromToken(String token)
+    {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
 }
