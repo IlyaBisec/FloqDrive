07c6a4a9c58b371e9be480d8e792ee2bdf728cd9
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 02:58:48 +0300

    feat: add global exception handler

--- src/main/java/com/floqdrive/exception/GlobalExceptionHandler.java
+++ src/main/java/com/floqdrive/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.floqdrive.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException exception)
+    {
+        return ResponseEntity.status(404).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException exception)
+    {
+        return ResponseEntity.status(400).body(exception.getMessage());
+    }
 }
