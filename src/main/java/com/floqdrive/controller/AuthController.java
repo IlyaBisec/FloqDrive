eae13c6f3bac5f6926bc1956cbf4ad6bf7f8d528
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 02:57:31 +0300

    feat: add auth controllrer

--- src/main/java/com/floqdrive/controller/AuthController.java
+++ src/main/java/com/floqdrive/controller/AuthController.java
@@ -1,4 +1,31 @@
 package com.floqdrive.controller;
 
-public class AuthController {
+import com.floqdrive.dto.LoginRequest;
+import com.floqdrive.dto.RegisterRequest;
+import com.floqdrive.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController
+{
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody @Valid RegisterRequest registerRequest)
+    {
+        authService.register(registerRequest);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody @Valid LoginRequest loginRequest)
+    {
+        return authService.login(loginRequest); // Return JWT
+    }
 }
