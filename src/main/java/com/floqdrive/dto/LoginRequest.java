ef1355d9c0161f381a4084a2d77e255be37f5111
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 02:58:18 +0300

    feat: add login request

--- src/main/java/com/floqdrive/dto/LoginRequest.java
+++ src/main/java/com/floqdrive/dto/LoginRequest.java
@@ -1,4 +1,18 @@
 package com.floqdrive.dto;
 
-public class LoginRequest {
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// For user login
+@Getter
+@Setter
+public class LoginRequest
+{
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
