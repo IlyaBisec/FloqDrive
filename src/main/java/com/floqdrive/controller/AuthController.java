5e4e6e7bb3b7e1b3db604d77235740c818224455
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 04:21:52 +0300

    fix: short import depedences

--- src/main/java/com/floqdrive/controller/AuthController.java
+++ src/main/java/com/floqdrive/controller/AuthController.java
@@ -3,12 +3,12 @@ package com.floqdrive.controller;
 import com.floqdrive.dto.LoginRequest;
 import com.floqdrive.dto.RegisterRequest;
 import com.floqdrive.service.AuthService;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
