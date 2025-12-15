4e46c0f975a0dfab508ec2f3d4dcd6c81863f937
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-12-15 04:24:10 +0300

    feat: add jwt filter

--- src/main/java/com/floqdrive/security/JwtFilter.java
+++ src/main/java/com/floqdrive/security/JwtFilter.java
@@ -1,4 +1,54 @@
 package com.floqdrive.security;
 
-public class JwtFilter {
+import com.floqdrive.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter
+{
+    private final JwtProvider jwtProvider;
+    // Service which load user by ID
+    private final CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException
+    {
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer "))
+        {
+            String token = header.substring(7);
+            Long userId = jwtProvider.getUserIdFromToken(token);
+
+            var userDetails = userDetailsService.loadUserById(userId);
+
+            var auth = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+            );
+
+            auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
 }
+
