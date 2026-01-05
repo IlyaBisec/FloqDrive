cd2099421c13f9699d1fc0622ac7946b68d2bd77
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2026-01-05 22:53:03 +0300

    feat: add swagger html

--- backend/src/main/java/com/floqdrive/security/SecurityConfig.java
+++ backend/src/main/java/com/floqdrive/security/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/error"
+                                "/error",
+                                "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
